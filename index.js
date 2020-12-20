d3fb7f51ebf25dd5a9771bcfe19c6fb01899300e
mac <mac@superMac.lan>
2020-12-20 15:43:09 +0800

    luna-utils

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-import _domainUtils from './src/domain';
-import _dynamicUtils from './src/dynamic';
-import _h5Env from './src/h5Env';
-import _deviceUtils from './src/device';
-import _authorizeUtils from './src/authorize';
-import _systemUtils from './src/system';
+import domainUtils from './src/domain';
+import dynamicUtils from './src/dynamic';
+import h5Env from './src/h5Env';
+import deviceUtils from './src/device';
+import authorizeUtils from './src/authorize';
+import systemUtils from './src/system';
 
-export const domainUtils = _domainUtils
-export const dynamicUtils = _dynamicUtils
-export const h5Env = _h5Env
-export const deviceUtils = _deviceUtils
-export const authorizeUtils = _authorizeUtils
-export const systemUtils = _systemUtils
+export const domainUtils = domainUtils
+export const dynamicUtils = dynamicUtils
+export const h5Env = h5Env
+export const deviceUtils = deviceUtils
+export const authorizeUtils = authorizeUtils
+export const systemUtils = systemUtils
