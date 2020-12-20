84828ced7a05855c61c27f4624307a0b5d71a170
mac <mac@superMac.lan>
2020-12-20 22:21:03 +0800

    fixed

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ import deviceUtils from './src/device';
 import authorizeUtils from './src/authorize';
 import systemUtils from './src/system';
 
-export const domainUtils = domainUtils
-export const dynamicUtils = dynamicUtils
-export const h5Env = h5Env
-export const deviceUtils = deviceUtils
-export const authorizeUtils = authorizeUtils
-export const systemUtils = systemUtils
+export {
+    domainUtils,
+    dynamicUtils,
+    h5Env,
+    deviceUtils,
+    authorizeUtils,
+    systemUtils
+}
