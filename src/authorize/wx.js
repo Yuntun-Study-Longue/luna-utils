c3fbee6a447f77fe26a882f0cb9cd3d08e96bcd0
Alichs <github@alichs.com>
2021-01-31 22:10:07 +0800

    fixed

--- src/authorize/wx.js
+++ src/authorize/wx.js
@@ -1,5 +1,6 @@
-import Promise from 'bluebird';
+import Promise, { resolve } from 'bluebird';
 import superagent from 'superagent';
+import qs from 'query-string';
 import _domainUtils from '../domain';
 
 export const wxThirdPartLogin = (scope = 'snsapi_userinfo', redirect_uri = window.location.href) =>
@@ -15,7 +16,6 @@ export const wxThirdPartLogin = (scope = 'snsapi_userinfo', redirect_uri = windo
         }
         const _redirect_uri = urlParamsStr ? redirect_uri.split('?')[0] + '?' + urlParamsStr : redirect_uri.split('?')[0];
         const url = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${app_id}&redirect_uri=${encodeURIComponent(_redirect_uri)}&response_type=code&scope=${scope}&state=${(new Date()).getTime()}#wechat_redirect`;
-        console.log(url, '=== url');
         window.location.href = url
     })
 
@@ -49,6 +49,20 @@ export const wxFetchBaseInfo = () =>
         superagent.get(`/webcore/wx/base/${web_app_id}/get_wx_openid`)
         .query({ code, type: "h5", scope: 'snsapi_base' })
         .then(res => {
+            if (!!(window.history && history.pushState)){
+                // 支持History API
+                // skipEmptyString: {a: 1, b: '', c: '', d: 4} => 'a=1&d=4'
+                // skipNull: {a: 1, b: undefined, c: null, d: 4} => 'a=1&d=4'
+                let qsObj = qs.parse(window.location.search, { skipNull: true , skipEmptyString: true });
+                delete qsObj.code && delete qsObj.state
+                const _search = qs.stringify(qsObj);
+                if (_search) {
+                    history.pushState(null, null, `${location.pathname}?${_search}` )
+                }
+                else {
+                    history.pushState(null, null, location.pathname)
+                }
+            }
             if (res.body.errcode === 40163 || res.body.errcode === 40029) {
                 // 此时代表code已被使用，需要重新授权
                 wxThirdPartLogin('snsapi_base')
@@ -58,4 +72,13 @@ export const wxFetchBaseInfo = () =>
             }
             else resolve(null);
         }).catch((e) => resolve(null))
+    })
+
+export const wxFetchUserInfoByOpenID = (OPEN_ID) =>
+    new Promise(resolve => {
+        const web_app_id = _domainUtils.getWebAppId()
+        superagent.get(`/webcore/wx/base/${web_app_id}/get_wx_userinfo_by_openid`)
+        .query({ OPEN_ID })
+        .then(res => resolve(res.body) )
+        .catch(e => resolve(null))
     })
